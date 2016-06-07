47373f461dff2f9c5d95611845d122a33deacffa
Stephen Hosom <0xhosom@gmail.com>
2016-06-07 10:54:33 -0400

    testing

--- filehash.py
+++ filehash.py
@@ -15,7 +15,7 @@ class HashMatch(BotPlugin):
 	def callback_message(self, msg):
 		'''Check the messages if they contain a hash.'''
 
-		if self.bot_identifier == msg.frm:
+		if self.bot_identifier == msg.frm.username:
 			return
 
 		for match in self.pattern.finditer(msg.body):
