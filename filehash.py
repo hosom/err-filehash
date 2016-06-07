737e8e6725d7464a6b40decd0dfafff44783e257
Stephen Hosom <0xhosom@gmail.com>
2016-06-07 14:39:10 -0400

    testing

--- filehash.py
+++ filehash.py
@@ -20,16 +20,6 @@ class HashMatch(BotPlugin):
 		if user == str(self.bot_identifier):
 			return
 
-		self.send(msg.to, "@%s" % (msg.frm.username))
-		self.send(msg.to, self.bot_identifier)
-		#if self.bot_identifier == user:
-		#	return
-
-		#if self.bot_identifier == str(msg.frm.username):
-		#	return
-
 		for match in self.pattern.finditer(msg.body):
-			self.send(msg.to, msg.frm)
-			self.send(msg.to, match.group(0))
-			self.log('Seems like a match: %s' % (match.group(0)))
+			self.send(msg.to, 'Seems like a match: %s' % (match.group(0)))
 		return
