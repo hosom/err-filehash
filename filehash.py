5c494416314dcbaa76e88c10a703691dbf01770f
Stephen Hosom <0xhosom@gmail.com>
2016-06-09 15:15:37 -0400

    add vtapi

--- filehash.py
+++ filehash.py
@@ -4,6 +4,7 @@ import dns.resolver
 from errbot import BotPlugin, botcmd, cmdfilter
 
 _MHR_API = 'malware.hash.cymru.com'
+_VTAPI = 'https://www.virustotal.com/vtapi/v2/'
 
 def mhr(ahash):
 	'''Lookup a file in the malware hash registry.'''
@@ -23,6 +24,18 @@ def mhr(ahash):
 									answer.detection_rate
 									)
 
+def file_report(ahash, api_key):
+	'''
+	'''
+
+	parameters = {
+				'resource' : ahash,
+				'apikey': api_key 
+				}
+	r = requests.post('%sfile/report' % (_VTAPI), 
+					data=parameters)
+	return r.json()
+
 class HashMatch(BotPlugin):
 	'''Plugin that finds file hashes inside of messages and then performs
 	lookups and actions based on the presence of a file hash.
@@ -34,6 +47,10 @@ class HashMatch(BotPlugin):
 		# for better performance.
 		self.pattern = re.compile('([a-fA-F0-9]{64}|[a-fA-F0-9]{40}|[a-fA-F0-9]{32})')
 
+	def get_configuration_template(self):
+		return {
+			'vt_apikey' : 'virustotal api key'
+		}
 
 	def callback_message(self, msg):
 		'''Check the messages if they contain a hash.'''
@@ -48,4 +65,15 @@ class HashMatch(BotPlugin):
 		for match in self.pattern.finditer(msg.body):
 			self.send(msg.to, 'Found a file hash: %s' % (match.group(0)))
 			self.send(msg.to, mhr(match.group(0)))
+
+			vtresult = file_report(match.group(0), self.config['vt_apikey'])
+			if vtresult['response_code'] == 1:
+				self.send(msg.to, '''
+```
+VirusTotal
+%s
+%s/%s
+```
+For more information see: %s
+''' % (vtresult['scan_date'], vtresult['positives'], vtresult['total'], vtresult['permalink']))
 		return
